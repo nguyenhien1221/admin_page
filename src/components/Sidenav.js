4c3087507d99fed60db09ad8ee6dea6d311fdbcc
NguyenHien <nguyenhien2942000@gmail.com>
2022-11-08 17:22:50 +0700

    add header and sidenav

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <div className="sidenav_wrapper">
+      <div className="sidenav_wrapper_control">
+        <ul className="list">
+          <li>
+            <a href="/">Dashboard</a>
+          </li>
+          <li>
+            <a href="/user">Users</a>
+          </li>
+          <li>
+            <a href="/">Setting</a>
+          </li>
+          <li>
+            <a href="/">Help</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidenav;
