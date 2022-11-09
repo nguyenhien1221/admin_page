2bc6f83eb1215d548cba5e97f4f8306f7566ef5a
NguyenHien <nguyenhien2942000@gmail.com>
2022-11-09 16:40:33 +0700

    update data in user page,add delete user feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'antd/dist/antd.css';
 import '../src/style/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import { Table } from 'antd';
+
+import { useState, useEffect } from 'react';
 
 const Users = () => {
-  return <div>Users</div>;
+  const { Column } = Table;
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+
+      setUser(data.splice(0,5));
+    };
+    getData();
+  }, []);
+
+  const handleDelete = (id) => {
+    const newListUser = user.filter((item) => item.id !== id);
+    setUser(newListUser);
+  };
+
+  return (
+    <div className="users_wrapper">
+      <p className="users_wrapper_title">Manage Users</p>
+      <div className="users_wrapper_table">
+        <Table className="table" dataSource={user}>
+          <Column title="Name" dataIndex="name" />
+          <Column title="User Name" dataIndex="username" />
+          <Column title="Email" dataIndex="email" />
+          <Column title="Phone" dataIndex="phone" />
+          <Column
+            title="Action"
+            key="id"
+            render={(item) => (
+              <button className="table_btn" onClick={() => handleDelete(item.id)}>
+                Delete
+              </button>
+            )}
+          />
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
